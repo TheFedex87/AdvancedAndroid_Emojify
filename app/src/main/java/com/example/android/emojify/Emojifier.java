97ae607dd6700a8393e22b99783c5e67b75f9329
Federico Creti <federico.creti87@outlook.it>
2018-03-11 11:41:15 +0100

    Exercise done

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -59,10 +59,18 @@ class Emojifier {
         }
 
         // TODO (2): Iterate through the faces, calling getClassifications() for each face.
+        for(int i = 0; i < faces.size(); i++){
+            getClassifications(faces.get(i));
+        }
 
         // Release the detector
         detector.release();
     }
 
     // TODO (1): Create a static method called getClassifications() which logs the probability of each eye being open and that the person is smiling.
+    static void getClassifications(Face face){
+        Log.d(LOG_TAG, "Probability left eye is open is: " + face.getIsLeftEyeOpenProbability());
+        Log.d(LOG_TAG, "Probability right eye is open is: " + face.getIsRightEyeOpenProbability());
+        Log.d(LOG_TAG, "Probability person is smiling is: " + face.getIsSmilingProbability());
+    }
 }
